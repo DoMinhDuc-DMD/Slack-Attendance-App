197587b51a40a0620f06f9b30c4bca46d6a20467
Đỗ Minh Đức <minhducdo202@gmail.com>
2025-08-18 13:44:22 +0700

    Update DB connection for server

--- services/createDbConnection.js
+++ services/createDbConnection.js
@@ -1,25 +1,25 @@
 const mysql = require('mysql2/promise');
 
 // Local
-async function createDbConnection() {
-    return await mysql.createConnection({
-        host: 'localhost',
-        port: 3308,
-        user: 'root',
-        password: 'password',
-        database: 'slackbot'
-    });
-}
-
-// Server
 // async function createDbConnection() {
 //     return await mysql.createConnection({
-//         host: process.env.DB_HOST || 'db',
-//         port: process.env.DB_PORT || 3306,
-//         user: process.env.DB_USER || 'slack',
-//         password: process.env.DB_PASSWORD || 'password',
-//         database: process.env.DB_NAME || 'slackbot'
+//         host: 'localhost',
+//         port: 3308,
+//         user: 'root',
+//         password: 'password',
+//         database: 'slackbot'
 //     });
 // }
+
+// Server
+async function createDbConnection() {
+    return await mysql.createConnection({
+        host: process.env.DB_HOST || 'db',
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USER || 'slack',
+        password: process.env.DB_PASSWORD || 'password',
+        database: process.env.DB_NAME || 'slackbot'
+    });
+}
 
 module.exports = { createDbConnection };
