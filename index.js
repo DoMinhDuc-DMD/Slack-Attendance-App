2815db69d6131af54d1357cab51ddd867b9a2486
Đỗ Minh Đức <minhducdo202@gmail.com>
2025-08-21 15:43:20 +0700

    Implement ngrok auth token

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const ngrok = require('ngrok');
+
+// load app + server
 require('./app');
 require('./server');
+
+(async function () {
+    try {
+        const url = await ngrok.connect({
+            addr: 3000,
+            authtoken: process.env.NGROK_TOKEN,
+        });
+        console.log("Ngrok tunnel running at:", url);
+    } catch (err) {
+        console.error("Failed to start ngrok:", err);
+    }
+})();
